131693a19e085610a54d1dd2bc06312d066ac617
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-04-13 19:54:00 +0300

    +

--- src/components/MoreModal.vue
+++ src/components/MoreModal.vue
@@ -1,43 +1,47 @@
 <template>
- <div
-  @click="store.closeModal"
-  v-if="store.moreModal.show"
-  class="fixed top-0 left-0 w-full h-dvh flex bg-[rgba(0,0,0,0.5)] backdrop-blur-sm z-[2000] overflow-auto p-[10px]"
- >
+ <transition name="more">
   <div
-   @click.stop
-   class="relative w-full max-w-[500px] flex flex-col m-auto bg-[#fff] p-[30px_10px_20px] rounded-[20px]"
+   @click="store.closeModal"
+   v-if="store.moreModal.show"
+   class="fixed top-0 left-0 w-full h-dvh flex bg-[rgba(0,0,0,0.5)] backdrop-blur-sm z-[2000] overflow-auto p-[10px]"
   >
-   <p class="text-[26px] font-700 text-center mb-[10px]">
-    {{ store.moreModal.data?.title }}
-   </p>
-   <img
-    class="self-center w-[150px] h-[150px] object-contain object-center mb-[20px]"
-    :src="store.moreModal.data?.image"
-    alt=""
-    loading="lazy"
-   />
-   <p class="text-[18px] mb-[20px]">{{ store.moreModal.data?.description }}</p>
-   <div class="flex items-center justify-between">
-    <span class="flex items-center gap-[5px]">
-     <span class="text-[30px]">{{ store.moreModal.data?.rating.rate }}</span>
-     <img
-      class="relative top-[-2px] w-[30px] aspect-square"
-      src="/src/assets/img/svg/star.svg "
-      alt=""
-     />
-    </span>
-    <span class="text-[30px] font-600">{{ store.moreModal.data?.price }}$</span>
-   </div>
-   <button
-    @click="store.closeModal"
-    class="absolute top-[8px] right-[15px]"
-    type="button"
+   <div
+    @click.stop
+    class="relative w-full max-w-[500px] flex flex-col m-auto bg-[#fff] p-[30px_10px_20px] rounded-[20px]"
    >
-    &#10006;
-   </button>
+    <p class="text-[26px] font-700 text-center mb-[10px]">
+     {{ store.moreModal.data?.title }}
+    </p>
+    <img
+     class="self-center w-[150px] h-[150px] object-contain object-center mb-[20px]"
+     :src="store.moreModal.data?.image"
+     alt=""
+     loading="lazy"
+    />
+    <p class="text-[18px] mb-[20px]">{{ store.moreModal.data?.description }}</p>
+    <div class="flex items-center justify-between">
+     <span class="flex items-center gap-[5px]">
+      <span class="text-[30px]">{{ store.moreModal.data?.rating.rate }}</span>
+      <img
+       class="relative top-[-2px] w-[30px] aspect-square"
+       src="/src/assets/img/svg/star.svg "
+       alt=""
+      />
+     </span>
+     <span class="text-[30px] font-600"
+      >{{ store.moreModal.data?.price }}$</span
+     >
+    </div>
+    <button
+     @click="store.closeModal"
+     class="absolute top-[8px] right-[15px]"
+     type="button"
+    >
+     &#10006;
+    </button>
+   </div>
   </div>
- </div>
+ </transition>
 </template>
 
 <script lang="ts">
@@ -57,3 +61,15 @@ export default defineComponent({
  },
 });
 </script>
+
+<style scoped>
+.more-enter-active,
+.more-leave-active {
+ transition: opacity 0.2s ease;
+}
+
+.more-enter-from,
+.more-leave-to {
+ opacity: 0;
+}
+</style>
