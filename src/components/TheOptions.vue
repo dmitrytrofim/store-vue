4abe9e437254c6a41895e3477c5233b7df08c54f
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-04-30 19:41:54 +0300

    +

--- src/components/TheOptions.vue
+++ src/components/TheOptions.vue
@@ -1,10 +1,15 @@
 <template>
- <div>test</div>
+ <div>
+  <Button>test</Button>
+ </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Button from 'primevue/button';
+
 export default defineComponent({
+ components: { Button },
  name: 'the-options',
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import '@/style.css';
 import App from '@/App.vue';
 import components from '@components/index';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -10,4 +11,5 @@ const pinia = createPinia();
 components.forEach((comp: any) => app.component(comp.name, comp));
 
 app.use(pinia);
+app.use(PrimeVue);
 app.mount('#app');
