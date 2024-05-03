d9bb7fa29b99011fd6136722202758a629420e62
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-05-03 09:55:26 +0300

    +

--- src/components/OrderModal.vue
+++ src/components/OrderModal.vue
@@ -7,7 +7,7 @@
    class="relative w-full max-w-[600px] bg-[white] rounded-[20px] p-[40px_20px_20px] m-[auto]"
   >
    <h2 class="text-[30px] font-600 text-center mb-[20px]">Your order</h2>
-   <ul class="flex flex-col gap-[15px]">
+   <ul class="flex flex-col gap-[15px] mb-[20px]">
     <li
      class="grid grid-cols-[auto_1fr_auto_auto_60px] items-center gap-[10px]"
      v-for="product in store.cart.products"
@@ -23,6 +23,16 @@
      <span class="font-600">{{ product.buy! * product.price }}$</span>
     </li>
    </ul>
+   <div class="flex justify-between items-center">
+    <span class="text-[24px] font-600"
+     >Total: {{ store.calcProductCart() }}$</span
+    >
+    <button
+     class="font-600 text-[24px] text-[white] p-[5px_10px] bg-[green] rounded-[5px]"
+    >
+     To pay
+    </button>
+   </div>
    <button class="absolute top-[8px] right-[15px] text-[24px]" type="button">
     &#10006;
    </button>
