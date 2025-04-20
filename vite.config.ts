823d4c166ca38d6567aadb312b77db1e8743d875
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2025-04-20 18:23:25 +0300

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: '/portfolio/works/store-vue/',
+ base: '/',
  plugins: [
   vue(),
   ViteAliases({
